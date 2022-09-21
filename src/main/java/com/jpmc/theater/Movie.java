7c38a50f34945f89cbc2c44c56d60ff2e0cc4ba1
sjlee2016 <sjlee2016@sogang.ac.kr>
2022-09-21 12:46:10 -0400

    Add Discount rule for 11 AM - 4PM and refactor

--- src/main/java/com/jpmc/theater/Movie.java
+++ src/main/java/com/jpmc/theater/Movie.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 import java.util.Objects;
 
 public class Movie {
-    private static int MOVIE_CODE_SPECIAL = 1;
 
     private String title;
     private String description;
@@ -31,33 +30,7 @@ public class Movie {
         return ticketPrice;
     }
 
-    public double calculateTicketPrice(Showing showing) {
-        return ticketPrice - getDiscount(showing.getSequenceOfTheDay());
-    }
-
-    private double getDiscount(int showSequence) {
-        double specialDiscount = 0;
-        if (MOVIE_CODE_SPECIAL == specialCode) {
-            specialDiscount = ticketPrice * 0.2;  // 20% discount for special movie
-        }
-
-        double sequenceDiscount = 0;
-        if (showSequence == 1) {
-            sequenceDiscount = 3; // $3 discount for 1st show
-        } else if (showSequence == 2) {
-
-            sequenceDiscount = 2; // $2 discount for 2nd show
-        }else if (showSequence == 7) {
-            sequenceDiscount = 1; // $1 discount for 7th show
-        }
-//        else {
-//            throw new IllegalArgumentException("failed exception");
-//        }
-
-        // biggest discount wins
-        return specialDiscount > sequenceDiscount ? specialDiscount : sequenceDiscount;
-    }
-
+    public int getSpecialCode() { return specialCode;}
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/com/jpmc/theater/MovieTests.java
+++ src/test/java/com/jpmc/theater/MovieTests.java
@@ -14,8 +14,7 @@ public class MovieTests {
     void specialMovieWith50PercentDiscount() {
         Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 1);
         Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.now()));
-        assertEquals(10, spiderMan.calculateTicketPrice(showing));
-
+        assertEquals(10, showing.calculateFee(1));
         System.out.println(Duration.ofMinutes(90));
     }
 }
