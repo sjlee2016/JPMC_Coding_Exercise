66d6a5b3713769895cd43ce9acea6ede215527a2
sjlee2016 <sjlee2016@sogang.ac.kr>
2022-09-21 21:40:34 -0400

    fix local date provider test

--- src/test/java/com/jpmc/theater/Object/LocalDateProviderTest.java
+++ src/test/java/com/jpmc/theater/Object/LocalDateProviderTest.java
@@ -3,9 +3,14 @@ package com.jpmc.theater.Object;
 import com.jpmc.theater.Object.LocalDateProvider;
 import org.junit.jupiter.api.Test;
 
+import java.time.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LocalDateProviderTest {
     @Test
     void testCurrentTime() {
+        assertEquals(LocalDate.now(), LocalDateProvider.singleton().getCurrentDate());
         System.out.println("current time is - " + LocalDateProvider.singleton().getCurrentDate());
     }
 }
